c28c092bfe24c2fc4e752264d276b555dcb988fa
Jay Boice <jboice@gmail.com>
2013-06-18 15:40:32 -0400

    Comment and clean up downloader

--- ap/downloader.rb
+++ ap/downloader.rb
@@ -21,11 +21,7 @@ module AP
   private
 
     def connect
-      begin
-        @ftp = Net::FTP.new(@crawler.ap_config['host'])
-      rescue Exception => e
-        @ftp = Net::FTP.new(@crawler.ap_config['host'])
-      end
+      @ftp = Net::FTP.new(@crawler.ap_config['host'])
       @ftp.login(@crawler.ap_config['user'], @crawler.ap_config['pass'])
       @ftp.passive = true
     end
@@ -37,9 +33,9 @@ module AP
     def download_state(state)
       ftp_dir = "/#{state}/dbready"
       local_dir = "#{@crawler.dir}/data/#{state}"
-
       FileUtils.makedirs(local_dir) unless File.exists?(local_dir)
 
+      # Downloaded files depend on parameters
       files = ["#{state}_Results.txt", "#{state}_Race.txt"]
       files += ["#{state}_Candidate.txt"] if @crawler.params[:initialize]
       files += ["#{state}_Results_D.txt", "#{state}_Race_D.txt"] if @crawler.params[:delegates]
@@ -53,19 +49,24 @@ module AP
 
         begin
           timeout(20) do
+            # Use the file's mtime on the ftp server to see if it changed before downloading
             old_tm = File.exists?("#{local_file}.mtime") ? File.read("#{local_file}.mtime") : nil
             new_tm = @ftp.mtime("#{ftp_dir}/#{file}").to_i.to_s
             next if new_tm == old_tm
 
             @ftp.getbinaryfile("#{ftp_dir}/#{file}", "#{local_file}", 1024)
 
+            # Hash the old and new files to see if they changed (oftentimes, files on the ftp server will have a new timestamp but be unchanged)
             old_md5 = File.exists?("#{local_file}.md5") ? File.read("#{local_file}.md5") : nil
             new_md5 = Digest::MD5.hexdigest(File.read(local_file))
             next if old_md5 == new_md5
 
+            # Record the downloaded files for processing by the importer
             @crawler.new_files << [local_file, new_tm, new_md5]
             @crawler.updated_states << state unless @crawler.updated_states.include?(state)
           end
+
+        # Trap timeouts and a transient ftp error seen on higher traffic nights so they don't halt the crawler
         rescue Exception, Timeout::Error => e
           if !e.to_s.include?("The system cannot find the file")
             @crawler.logger.err "FTP ERR for #{ftp_dir}/#{file}: #{e.to_s}"
