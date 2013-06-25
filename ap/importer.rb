3d4692f5449a5dd46b9867dd45a07c72ed6489ae
Jay Boice <jboice@gmail.com>
2013-06-24 21:24:57 -0400

    Move create table sql

--- ap/importer.rb
+++ ap/importer.rb
@@ -159,7 +159,7 @@ module AP
     end
 
     def create_tables
-      system  "mysql -h #{@db_config["host"]} -u #{@db_config["username"]} --password=#{@db_config["password"]} #{@db_config["database"]} < #{@crawler.dir}/ap/create.sql"
+      system  "mysql -h #{@db_config["host"]} -u #{@db_config["username"]} --password=#{@db_config["password"]} #{@db_config["database"]} < #{@crawler.dir}/data/create_tables.sql"
     end
 
     def connect
